bd526d244c698917564703aba1e2cadf0111564f
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2022-11-20 13:12:17 +0100

    Create scrapper.py

---
+++ scrapper.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+import os
+import base64
+import plost
+import requests
+import io
+import xlsxwriter
+from io import BytesIO
+import altair as alt
+from bs4 import BeautifulSoup
+import lxml
+import json
+import BVCscrap  as bvc
+
+recap=bvc.getIndexRecap()
+st.text('onga bounga')
