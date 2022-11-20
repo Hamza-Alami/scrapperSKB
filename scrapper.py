d83e91b17c265062228b555bca7df4297179fb6b
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2022-11-20 21:26:54 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -72,9 +72,76 @@ def usdmad():
         
     usd = float(dataus.decode()[-25:-18])
     return usd
-#BAMcc = pd.DataFrame({'Cours en MAD': [euromad(), usdmad()]},index=['EUR', 'USD'])
-#st.dataframe(BAMcc)
+BAMcc = pd.DataFrame({'Cours en MAD': [euromad(), usdmad()]},index=['EUR', 'USD'])
+st.dataframe(BAMcc)
 
+#Scrap from yahoo finance
+
+#                                        Indices
+def indices():
+    #Dow jones
+    dj30 = yf.Ticker("^DJI")
+    dj30 = dj30.info['previousClose']
+
+    #spoos
+    sp500 = yf.Ticker("^GSPC")
+    sp500 = sp500.info['previousClose']
+
+    #nasdaq
+    nasdaq = yf.Ticker("^IXIC")
+    nasdaq = nasdaq.info['previousClose']
+
+    #cac40
+    cac = yf.Ticker("^FCHI")
+    cac = cac.info['previousClose']
+
+    #DAX
+    dax = yf.Ticker("^GDAXI")
+    dax = dax.info['previousClose']
+
+    #nikkei
+    jp = yf.Ticker("^N225")
+    jp = jp.info['previousClose']
+
+    #hangseng
+    hk = yf.Ticker("^HSI")
+    hk = hk.info['previousClose']
+    
+    return dj30, sp500, nasdaq, cac, dax, jp, hk
+
+
+#                                        Commodities
+
+
+def commodities():
+    #Gold
+    gld = yf.Ticker("GC=F")
+    gld = gld.info['previousClose']
+
+    #Brent
+    oil = yf.Ticker("BZ=F")
+    oil = oil.info['previousClose']
+
+    #silver
+    silver = yf.Ticker("SI=F")
+    silver = silver.info['previousClose']
+    return gld, oil, silver
+
+#                                          FX
+
+#eurodollar
+eurusd = yf.Ticker("EURUSD=X")
+eurusd = eurusd.info['previousClose']
+
+#calling funcs to lists
+indiceslist = indices()
+commolist = commodities()
+
+FXCOM = pd.DataFrame({'Cours': [eurusd, commolist[1], commolist[0], commolist[2]]},index=['EUR/USD','Brent', 'Gold', 'Silver'])
+intlindices = pd.DataFrame({'Cours': [indiceslist[0], indiceslist[1], indiceslist[2], indiceslist[3], indiceslist[4], indiceslist[5], indiceslist[6]]},index=['Dow Jones','S&P500', 'Nasdaq', 'CAC40', 'DAX30', 'NIKKEI','Hang Seng'])
+
+st.dataframe(FXCOM)
+st.dataframe(intindices)
 #BVCscrapper
 
 #loading indices
