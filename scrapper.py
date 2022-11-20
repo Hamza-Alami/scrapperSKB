b8392bfbbf3e73594a9f72f2bd46daaacbaa5477
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2022-11-20 17:20:27 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,22 +1,4 @@
 import streamlit as st
-import numpy as np
-import datetime
-import matplotlib.pyplot as plt
-import os
-import base64
-import plost
-import requests
-import io
-import xlsxwriter
-from io import BytesIO
-import altair as alt
-from bs4 import BeautifulSoup
-import lxml
-import BVCscrap  as bvc
-import http.client, urllib.request, urllib.parse, urllib.error, base64
-import yfinance as yf
-import BVCscrap as load
-import pandas as pd
 
 #recap=bvc.getIndexRecap()
 st.text('onga bounga')
