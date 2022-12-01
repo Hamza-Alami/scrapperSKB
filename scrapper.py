66218f4b02f77e443dd9c3741d0251efcc0583eb
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2022-12-01 20:03:46 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -178,10 +178,13 @@ st.write(recap['Volume Global'])
 tickerlist = bvc.notation()
 #['Addoha','AFMA','Afric Indus','Afriquia Gaz','Agma','Alliances','Aluminium Maroc','Aradei Capital','ATLANTASANAD','Attijariwafa','Auto Hall','Auto Nejma','BALIMA','BOA','BCP','BMCI','Cartier Saada','CDM','CIH','Ciments Maroc','CMT','Colorado','COSUMAR','CTM','Dari Couspate','Delattre Lev','Delta Holding','DISWAY','Ennakl','EQDOM','FENIE BROSSETTE','HPS','IBMaroc','Immr Invest','INVOLYS','Jet Contractors','LABEL VIE','LafargeHolcim','Lesieur Cristal','Lydec','M2M Group','Maghreb Oxygene','Maghrebail','Managem','Maroc Leasing','Maroc Telecom','Med Paper','Microdata','Mutandis','Oulmes','PROMOPHARM','Rebab Company','Res.Dar Saada','Risma','S2M','Saham Assurance','SALAFIN','SMI','Stokvis Nord Afr','SNEP','SODEP','Sonasid','SRM','Ste Boissons','STROC Indus','TAQA Morocco','Timar','Total Maroc','Unimer','SOTHEMA','Wafa Assur','Zellidja']
 datafr = pd.DataFrame (tickerlist, columns = ['Valeur'])
+datafr["Cours"] = bvc.loadata(datafr["Valeur"],start='2022-12-01',end='2022-12-01')
+st.write(datafr)
 
 #for i in tickerlist :
     #seance=bvc.loadata(i,start='2022-11-25',end='2022-11-28')
     #framed = pd.DataFrame(seance)
     #framed['Ticker'] = i
     #fdf = st.dataframe(framed)
-st.write(datafr)
+
+
