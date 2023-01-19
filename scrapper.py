74612ee598f869bc0e7db5920e078608ff450d32
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-01-19 17:31:14 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import datetime as dt
+from datetime import timedelta
 import matplotlib.pyplot as plt
 import os
 import base64
