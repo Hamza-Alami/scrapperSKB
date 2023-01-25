f765d3493a4530446fc75aade04e2be21f9979a0
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-01-25 11:11:38 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -96,11 +96,11 @@ def usdmad():
     usdt1 = usdt[0]
     for i, val in enumerate(usdt1):
         sep = ','
-        strippedx = val["datetime"].split(sep, 1)[0]
-        thingy.append({'Ticker': val["name"],'Cours': val["cours"], 'Cloture': val["cloture"],'Variation': val["variation"], 'Volume Titre': val["volumeTitre"],"derniere transaction" : strippedx})    
+        #strippedx = val["datetime"].split(sep, 1)[0]
+        #thingy.append({'Ticker': val["name"],'Cours': val["cours"], 'Cloture': val["cloture"],'Variation': val["variation"], 'Volume Titre': val["volumeTitre"],"derniere transaction" : strippedx})    
     
     #float(dataus.decode()[-25:-18])
-    return usdt, usdt1, thingy
+    return usdt, usdt1#, #thingy
 
 BAMcc = pd.DataFrame({'Cours en MAD': [euromad(), usdmad()]},index=['EUR', 'USD'])
 
