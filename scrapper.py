11032d02c056fb95c3266c68c264e8f2aee727e7
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-01-30 12:58:21 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -394,6 +394,12 @@ df_merge_2 = pd.merge(courspond, df_merge_col, on='BVC')
 df_merge_2['Cloture'] = df_merge_2['Cloture'].apply(lambda x: x.replace(" ", ""))
 df_merge_2['Cours'] = df_merge_2['Cours'].apply(lambda x: x.replace(".", ","))
 
+df_merge_2['Cours'] = df_merge_2['Cours'].astype(float)
+df_merge_2['Cloture'] = df_merge_2['Cloture'].astype(float)
+df_merge_2['Variation'] = df_merge_2['Variation'].astype(float)
+df_merge_2['Volume Titre'] = df_merge_2['Volume Titre'].astype(float)
+
+
 
 
 st.write(df_merge_2)
