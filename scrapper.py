9193c9a81de9280196f415f289769998e7f705af
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-01-30 14:23:28 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -108,8 +108,8 @@ def euromad():
         
     eur = data.decode()
     euro = json.loads(eur)
-    euro1 = euro[0]
-    eurmad = euro1.get("moyen")
+    #euro1 = euro[0]
+    #eurmad = euro1.get("moyen")
     return euro #, eurmad2, eurmad3
 
 def usdmad():
