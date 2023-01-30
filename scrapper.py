914523a8b44c68fe516728df62742f92ae6eee0b
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-01-30 14:20:03 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -344,7 +344,7 @@ st.dataframe(dfindex)
 st.text('Indices sectoriaux')
 dfsect['Var%'] = dfsect['Var%'].apply(lambda x: x.replace(" %", ""))
 dfsect['Var% 31/12'] = dfsect['Var% 31/12'].apply(lambda x: x.replace(" %", ""))
-dfsect['Var% 31/12'] = dfsect['Var% 31/12'].astype(float)
+#dfsect['Var% 31/12'] = dfsect['Var% 31/12'].astype(float)
 #dfsect['Var%'] = dfsect['Var%'].astype(float)
 
 st.dataframe(dfsect)
