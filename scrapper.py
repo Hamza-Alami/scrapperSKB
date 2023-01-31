ade41e60db99ed4282994bdfe21c2ca120337ecf
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-01-31 10:26:07 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -308,9 +308,9 @@ commolist = commodities()
 
 #putting data into lists
 Cours1 = [indiceslist[0], indiceslist[1], indiceslist[2], indiceslist[3], indiceslist[4], indiceslist[5], indiceslist[6]]
-var1 = [indiceslist[7], indiceslist[8], indiceslist[9], indiceslist[10], indiceslist[11], indiceslist[12]]
+var1 = [indiceslist[7], indiceslist[8], indiceslist[9], indiceslist[10], indiceslist[11], indiceslist[12], indiceslist[13]]
 
-vari = [indiceslist[19], indiceslist[20], indiceslist[21], indiceslist[22], indiceslist[23], indiceslist[24]]
+vari = [indiceslist[19], indiceslist[20], indiceslist[21], indiceslist[22], indiceslist[23], indiceslist[24], indiceslist[25]]
 
 Cours2 =  [eurusd, commolist[1], commolist[0], commolist[2]]
 var2 =  [eurusdvar, commolist[3], commolist[4], commolist[5]]
