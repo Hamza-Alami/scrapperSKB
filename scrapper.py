59a840feed54e2144125d9017a1209dffdef8b3b
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-01-31 14:00:58 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -254,7 +254,7 @@ def indices():
     hkeoye = hkeoy.Close[0]
     hkvarytd = ((hk-hkeoye)*100)/hkeoye
     
-    return dj30, sp500, nasdaq, cac, dax, jp, hk, dj30var, sp500var, nasdaqvar, cacvar, daxvar, jpvar, hkvar, djeoye, speoye, naseoye, caceoye, daxeoye, jpeoye, hkeoye, djvarytd, spvarytd, nasvarytd, cacvarytd, daxvarytd, jpvarytd, hkvarytd
+    return dj30, sp500, nasdaq, cac, dax, jp, hk, dj30var, sp500var, nasdaqvar, cacvar, daxvar, jpvar, hkvar, djeoye, speoye, naseoye, caceoye, daxeoye, jpeoye, hkeoye, djvarytd, spvarytd, nasvarytd, cacvarytd, daxvarytd, jpvarytd, hkvarytd, dj
 
 #                                        Commodities
 
@@ -329,6 +329,7 @@ st.text('FX & commodities')
 st.dataframe(FXCOM)
 
 st.text('Indices internationaux')
+st.write(indiceslist[27])
 st.dataframe(intlindices)
 
 #BVCscrapper
