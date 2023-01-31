e4c364b852e9e310dc66212bb0e2cd2b2d411e40
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-01-31 14:18:51 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -42,7 +42,7 @@ if no == 0 :
     prevdate = lyoum - datetime.timedelta(days=4)
 elif no == 1 :
     sdate = lyoum - datetime.timedelta(days=2)
-    prevdate = lyoum - datetime.timedelta(days=3)
+    prevdate = lyoum - datetime.timedelta(days=4)
 else:
      sdate = lyoum - datetime.timedelta(days=1)
      prevdate = lyoum - datetime.timedelta(days=2)
