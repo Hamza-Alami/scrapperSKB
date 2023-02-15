e5cb2964b92d79a923fcaab521090bc25129e843
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-02-15 16:18:20 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -494,6 +494,6 @@ bingo = pd.DataFrame(notation, columns=['ticker'])
 test = bvc.getPond()
 test2 = pd.DataFrame.from_dict(test)
 test3 = test2.Instrument
-test3['test'] = bvc.getCours(test3['Instrument'])
+test3['test'] = bvc.getCours(test3.Instrument)
 
 st.write(test3)
