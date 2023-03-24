14e31c11e931aff5b5621c6d6fe06e6a9ff13af7
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-03-24 14:26:13 +0000

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -613,12 +613,10 @@ voldf = s.to_frame()
 
 url = "https://medias24.com/content/api?method=getAllStocks&format=json"
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
-}
+ua = UserAgent()
+headers = {'User-Agent': ua.random}
 
 response_API = requests.get(url, headers=headers)
-
 
 x = response_API.content
 st.write(x)
