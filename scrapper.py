a9ae7117472cbbd26cb4e3be2393bac26db21512
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-03-27 09:59:54 +0000

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -610,9 +610,9 @@ voldf = s.to_frame()
 
 #Scraping stock data from le Boursier 
 
-response_API = requests.get('https://medias24.com/content/api?method=getAllStocks&format=json')
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+response_API = requests.get('https://medias24.com/content/api?method=getAllStocks&format=json', headers=headers)
 x = response_API.content
-
 
 y = json.loads(x)
 z = y['result']
